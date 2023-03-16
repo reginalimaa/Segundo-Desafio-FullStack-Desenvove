62f0dfef2a61a8b90209c452bfe6922b6aba8a4d
reginalimaa <mariardlima@fac.pe.senac.br>
2023-03-16 01:23:05 -0300

    primeiro teste pwa

---
+++ assets/js/service-worker.js
@@ -0,0 +1,67 @@
+var cacheName = 'pwaTeste+-v1.2';
+
+self.addEventListener('install', event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+
+        './index.html',
+
+        './assets/css/reset.css',
+
+        './assets/css/style.css',
+
+        './assets/css/header.css',
+
+        './assets/css/main.css',
+
+        './assets/css/footer.css',
+
+        './assets/css/respnsive.css',
+
+        './assets/img/logo.png',
+        './assets/img/imgSection1.png',
+        './assets/img/imgSection2.png',
+        './assets/img/Avatar1.png',
+        './assets/img/Avatar2.png',
+        './assets/img/Avatar3.png',
+        './assets/img/imgSection3.png',
+        './assets/img/logo.png',
+        './assets/img/facebook.png',
+        './assets/img/linkedin.png',
+        './assets/img/Twitter.png',
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+     try {
+       return await fetch(event.request);
+     } catch (err) {
+       return caches.match(event.request);
+     }
+   }());
+
+  //Atualizacao cache
+  /*event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );*/
+
+});
