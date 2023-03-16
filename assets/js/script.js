13e15ae2061f531daac96f5c5b460557922e5e3e
reginalimaa <mariardlima@fac.pe.senac.br>
2023-03-15 22:37:32 -0300

    organizando pastas pastas. inicio da aplicacao PWA

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function sendForms() { 
+    
+    var userNome = document.getElementById("nameid");
+
+    if (userNome.value != "") {
+        alert('Obrigada sr(a)' + userNome.value + 'os seus dados foram caminhados com sucesso');
+    }
+}
+
+function isEmailCorrect(email){
+    var emailPattern = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
+    return emailPattern.test(email);
+}
+
+function sendForms(){
+    
+    var userEmail = document.getElementById("email").value;
+    console.log($("#email"));
+    console.log(document.getElementById("email"));
+    
+    var userMessage = document.getElementById("message").value;
+
+    if (isEmailCorrect(userEmail)){
+        var send_message = {
+            "email": userEmail,
+            "message": userMessage
+        }
+
+        console.log(send_message)
+    } else {
+        alert("email está incorreto");
+    }
+}
+
+
